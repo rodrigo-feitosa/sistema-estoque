4f2d9da8e2e1af06837812fbe6efe9f89ba393a4
Imdubitat <rodrigooliveirafeitosa@gmail.com>
2025-07-25 20:49:50 -0300

    início da classe produto

---
+++ js/script.js
@@ -0,0 +1,14 @@
+class produto {
+    constructor(nome, descricao, categoria, unidade_medida, preco, fornecedor) {
+        this.nome = document.getElementById('nome_produto');
+        this.descricao = document.getElementById('descricao_produto');
+        this.categoria = document.getElementById('categoria_produto');
+        this.unidade_medida = document.getElementById('unidade_medida');
+        this.preco = document.getElementById('preco_produto');
+        this.fornecedor = document.getElementById('fornecedor');
+    }
+
+    cadastrarProduto() {
+        
+    }
+}
